195b125d52e1f37c1ad254828415d1bff3a3b5d1
wangruigen <ruigen.wang@rails.cn>
2020-07-31 17:29:19 +0800

    xxx

---
+++ netty-client/src/main/java/com/xiaowang/netty/NettyServer.java
@@ -0,0 +1,8 @@
+package com.xiaowang.netty;
+
+public class NettyServer {
+
+    public static void main(String[] args) {
+        System.out.println("xx");
+    }
+}
---
+++ netty-server/src/main/java/com/xiaowang/netty/NettyClient.java
@@ -0,0 +1,8 @@
+package com.xiaowang.netty;
+
+public class NettyClient {
+
+    public static void main(String[] args) {
+        System.out.println("xx");
+    }
+}
